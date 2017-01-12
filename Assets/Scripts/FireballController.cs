b802fde96d06ba0c5be9b2b2724b4a9623fbe424
mdeniaud <mathieu.deniaud@crossknowledge.com>
2017-01-12 17:39:27 +0100

    Add standard assets

---
+++ Assets/Scripts/FireballController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FireballController : MonoBehaviour {
+
+    private Text debugText;
+
+	// Use this for initialization
+	void Start () {
+        debugText = GameObject.FindGameObjectWithTag("Debug").GetComponent<Text>();
+        debugText.text = "Fireball";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionExit()
+    {
+        debugText.text = "Collision fireball";
+        Destroy(gameObject, 0.0f);
+    }
+}
