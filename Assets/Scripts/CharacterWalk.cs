f0f7db5c327a23629b84fb2a215dae4367830b4a
mdeniaud <mathieu.deniaud@crossknowledge.com>
2017-03-21 14:24:09 +0100

    animation update

---
+++ Assets/Scripts/CharacterWalk.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterWalk : MonoBehaviour {
+
+    private Animator anim;
+    private GameObject character;
+    //public Text debug;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        //debug.text = "Start!";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        bool move = Input.GetKey(KeyCode.Z);//Input.GetAxis("Vertical");
+        if (move)
+        {
+            //debug.text = "Z key: " + move;
+            anim.SetFloat("speed", 1.0f);
+            Vector3 newPos = anim.transform.position;
+            newPos.z += 2.0f * Time.deltaTime;
+            anim.transform.position = newPos;
+        }
+        else
+        {
+            anim.SetFloat("speed", 0.0f);
+            //debug.text = "Z key(not): " + move;
+        }
+    }
+}
---
+++ Assets/Scripts/TPSmouseLook.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPSmouseLook : MonoBehaviour {
+
+    private Vector2 mouseLook;
+    private Vector2 smoothV;
+    private GameObject character;
+
+    public Transform targetLock;
+    public float sensivity = 5.0f;
+    public float smoothing = 2.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Quaternion rotation = Quaternion.LookRotation(targetLock.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * sensivity);
+    }
+}
