80e78c601f2567dfe0db4034b9cef501fa19f295
mdeniaud <mathieu.deniaud@crossknowledge.com>
2017-03-24 13:44:55 +0100

    Walking animation and script updated

--- Assets/Scripts/CharacterWalk.cs
+++ Assets/Scripts/CharacterWalk.cs
@@ -8,6 +8,7 @@ public class CharacterWalk : MonoBehaviour {
     private Animator anim;
     private Rigidbody rbody;
     private GameObject character;
+    private Vector3 lastCharacterLocalEulerAngles;
 
     public Transform cam;
 
@@ -17,6 +18,7 @@ public class CharacterWalk : MonoBehaviour {
     void Start () {
         anim = GetComponent<Animator>();
         rbody = GetComponent < Rigidbody>();
+        lastCharacterLocalEulerAngles = transform.localEulerAngles;
         //debug.text = "Start!";
     }
 
@@ -24,13 +26,38 @@ public class CharacterWalk : MonoBehaviour {
     void Update() {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+        bool moveForward = Input.GetKey(KeyCode.Z);
+        bool moveBackward = Input.GetKey(KeyCode.S);
+        bool moveLeft = Input.GetKey(KeyCode.Q);
+        bool moveRight = Input.GetKey(KeyCode.D);
         //debug.text = "Z key: " + move;
         anim.SetFloat("speed", 1.0f);
         
-        bool move = Input.GetKey(KeyCode.Z);//Input.GetAxis("Vertical");
+        bool move = moveForward || moveBackward || moveLeft || moveRight;
+
         if (move)
         {
-            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, cam.localEulerAngles.y, transform.localEulerAngles.z);
+
+            if (moveBackward)
+            {
+                transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, cam.localEulerAngles.y + 180, transform.localEulerAngles.z);
+                //transform.Translate(-3 * Vector3.forward * Time.deltaTime);
+            }
+            else if (moveLeft)
+            {
+                transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, cam.localEulerAngles.y - 90, transform.localEulerAngles.z);
+                //transform.Translate(3 * Vector3.left * Time.deltaTime);
+            }
+            else if (moveRight)
+            {
+                transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, cam.localEulerAngles.y + 90, transform.localEulerAngles.z);
+                //transform.Translate(3 * Vector3.left * Time.deltaTime);
+            }
+            else if(moveForward)
+            {
+                transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, cam.localEulerAngles.y, transform.localEulerAngles.z);
+                //transform.Translate(3 * Vector3.forward * Time.deltaTime);
+            }
             transform.Translate(3 * Vector3.forward * Time.deltaTime);
         }
         else
